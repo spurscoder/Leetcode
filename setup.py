9bca464cc9fc5a90c42aba30feaeda37020e683b
wangjiping55555 <wangjiping55555@gmail.com>
2018-10-24 10:46:51 +0800

    add

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="Leetcode",
+    description="Leetcode for test",
+    keywords=["good", "bad"],
+    version="0.0",
+    url = 'https://github.com/spurscoder/Leetcode/',
+    packages=["src"],
+    install_requires=[
+    ]
+)
+
